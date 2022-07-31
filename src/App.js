b85a8e4701eca5f782b7f910532e59a12c58d8fc
Shivam <shivampatel2473@gmail.com>
2022-07-30 21:45:43 -0400

    initialize the frontend

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import {
+    BrowserRouter as Router,
+    Route,
+    Navigate,
+    Routes,
+} from "react-router-dom";
+
+import "./App.css";
+import LoginPage from "./authPages/LoginPage/LoginPage";
+import RegisterPage from "./authPages/RegisterPage/RegisterPage";
+import Dashboard from "./Dashboard/Dashboard";
+
+function App() {
+    return (
+        <>
+            <Router>
+                <Routes>
+                    <Route exact path="/login">
+                        <LoginPage />
+                    </Route>
+                    <Route exact path="/register">
+                        <RegisterPage />
+                    </Route>
+                    <Route exact path="/dashboard">
+                        <Dashboard />
+                    </Route>
+                    <Route path="/">
+                        <Navigate to="/dashboard" />
+                    </Route>
+                </Routes>
+            </Router>
+        </>
+    );
+}
+
+export default App;
