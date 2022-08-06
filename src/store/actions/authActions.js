3c63a64b8c1c7d8bdb3074f490d72b2c6d5b8e3c
Shivam <shivampatel2473@gmail.com>
2022-08-05 22:29:12 -0400

    display server messages as alerts and add jwt token to requests

--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -1,5 +1,5 @@
 import * as api from "../../api";
-// import { openAlertMessage } from "./alertActions";
+import { openAlertMessage } from "./alertActions";
 
 export const authActions = {
     SET_USER_DETAILS: "AUTH.SET_USER_DETAILS",
@@ -26,7 +26,7 @@ const login = (userDetails, navigate) => {
         const response = await api.login(userDetails);
         console.log(response);
         if (response.error) {
-            // dispatch(openAlertMessage(response?.exception?.response?.data));
+            dispatch(openAlertMessage(response?.exception?.response?.data));
         } else {
             const { userDetails } = response?.data;
             localStorage.setItem("user", JSON.stringify(userDetails));
@@ -42,7 +42,7 @@ const register = (userDetails, navigate) => {
         const response = await api.register(userDetails);
         console.log(response);
         if (response.error) {
-            // dispatch(openAlertMessage(response?.exception?.response?.data));
+            dispatch(openAlertMessage(response?.exception?.response?.data));
         } else {
             const { userDetails } = response?.data;
             localStorage.setItem("user", JSON.stringify(userDetails));
